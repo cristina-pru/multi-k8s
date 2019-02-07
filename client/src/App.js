df9779d5a42b7deb2d3b397f79e3d565fe98303d
Cristina Prunescu <cristina.prunescu@itsystems.ch>
2019-02-07 17:37:48 +0100

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <Link to="/">Fib Calculator version 2</Link>
+            <Link to="/">Fib Calculator version KUBERNETES</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
